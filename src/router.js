f66cf17769cd8692871762101216e9dceea35db8
hodamansour <hodamansour164@gmail.com>
2019-04-18 10:34:16 +0300

    fixed issues number 9,10,11

--- src/router.js
+++ src/router.js
@@ -1,13 +1,20 @@
 const fs = require ('fs');
 const path = require('path');
-const handlers = require('./handlers');
+const handler = require('./handler');
+const querystring = require('querystring');
+// const filePath = path.join(__dirname, "..", "src",'word.txt');
+
 const router = (request, response) => {
   const url = request.url;
   if (url === '/') {
-    handlers.handler(request, response);
+   handler.handlerHome(request, response);
   } else if (url.indexOf('/public') !== -1) {
-    handlers.handler(request, response, url);
+    handler.handlerPublic(request, response, url);
+  }
+  else if (url.indexOf('/search?q=') !== -1) {
+   handlers.handlerSearch(request, response, url);
   } else {
+    handler.assets(url, response);
     response.writeHead(404, {'Content-Type': 'text/html' });
     response.end('<h1> 404 not found </h1>');
   }
---
+++ src/testing/test.js
@@ -0,0 +1,13 @@
+var test = require("tape");
+var main = require("../public/main.js");
+test("tape is working", function(t) {
+  t.equal(1, 1, "Should return 1 when given 1");
+  t.end();
+});
+
+test("return string when given a word", function(t) {
+  var actual = main("flower");
+  var expected = "flower";
+  t.deepEqual(actual, expected, "should return a string");
+  t.end();
+});
